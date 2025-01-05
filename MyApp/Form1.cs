e42e2fff87f3edd4c9df9afde35c7bfca9d277d6
lavindu perera <lavi156perera@gmail.com>
2025-01-05 23:41:22 +0530

    first commit

---
+++ MyApp/Form1.cs
@@ -0,0 +1,25 @@
+namespace MyApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rectangle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
